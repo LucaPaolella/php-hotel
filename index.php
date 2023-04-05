b645cfb4a257e48732d1e9028aeaca0f4a5b7ffc
Your Name <you@example.com>
2023-04-05 16:08:59 +0200

    table + bootstrap

--- index.php
+++ index.php
@@ -77,29 +77,55 @@ $hotels = array(
     ),
 );
 
-foreach ($hotels as $hotel) {
+/*foreach ($hotels as $hotel) {
     echo '<h2>' . $hotel['name'] . '</h2>';
     echo '<p>' . $hotel['description'] . '</p>';
     echo '<p>Location: ' . $hotel['parking'] . '</p>';
     echo '<p>Rating: ' . $hotel['vote'] . '</p>';
     echo '<p>Price: $' . $hotel['distance_to_center'] . '</p>';
-}
+}*/
 ?>
 
 
 <!--HTML-->
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotels</title>
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
+    <div class="container">
+        <h1>Hotels</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Parking</th>
+                    <th>Rating</th>
+                    <th>Distance to Center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) { ?>
+                    <tr>
+                        <td><?php echo $hotel['name']; ?></td>
+                        <td><?php echo $hotel['description']; ?></td>
+                        <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
+                        <td><?php echo $hotel['vote']; ?></td>
+                        <td><?php echo $hotel['distance_to_center']; ?> km</td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
